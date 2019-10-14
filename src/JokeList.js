52979bb84758071353643ff73768a63aa9770efc
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-14 13:43:24 +0530

    Adding dynamic colors and emojis
    Adding dynamic colors and emojis,Syncing up with local storage,Preventing duplicate jokes,Adding loading spinner.

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,42 +1,93 @@
 import React, {Component} from "react";
 import axios from "axios";
+import uuid from "uuid/v4"
 import Joke from "./Joke"
 import './JokeList.css';
 class JokeList extends Component{
-  // Default props
+// Default props
+
   static defaultProps = {
     numJokesToGet:10
   };
 
-  // constructor
+// constructor
+
   constructor(props){
     super(props);
     this.state = {
-      jokes:[]
+      jokes:JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+      loading:false
     };
+    this.seenJokes = new Set(this.state.jokes.map(j=>j.text));
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  // make requests to api under componentDidMount()
+// make requests to api under componentDidMount()
+
   async componentDidMount(){
-    //load jokes-make request using axios
+    if(this.state.jokes.length===0) this.getJokes();
+
+  }
+
+// Seperate method to get jokes
+
+  async getJokes(){
     let jokes = [];
     while(jokes.length < this.props.numJokesToGet){
       let res = await axios.get("https://icanhazdadjoke.com/",{
         headers:{Accept:"application/json"}
       });
-      jokes.push({text:res.data.joke,votes:0})
+      let newJoke = res.data.joke;
+      if(!this.seenJokes.has(newJoke)){
+        jokes.push({id:uuid(),text:newJoke,votes:0})
+        console.log("fvyhfjfjfcd");
+      }else{
+        console.log("Found duplicate!");
+        console.log(newJoke);
+      }
     }
-    this.setState({jokes:jokes});
+    this.setState(
+      st=>({
+        loading:false,
+        jokes:[...st.jokes, ...jokes]
+      }),
+      ()=>window.localStorage.setItem("jokes",JSON.stringify(this.state.jokes))
+    );
+  }
 
+// Function to handle clicks on button to get new Jokes
 
+  handleClick(){
+    this.setState({loading:true},this.getJokes);
+
+  }
+
+// Function to handle upvotes and downvotes
+
+  handleVote(id,delta){
+    this.setState(st => ({
+      jokes:st.jokes.map(j=>
+        j.id===id?{...j,votes:j.votes+delta}:j
+      )
+    }),
+    ()=>window.localStorage.setItem("jokes",JSON.stringify(this.state.jokes))
+    );
   }
 
 
 
-
   render(){
+    if(this.state.loading){
+      return(
+        <div className="JokeList-spinner">
+          <i className="far fa-8x fa-laugh fa-spin"/>
+          <h1 className="JokeList-title">Loading......</h1>
+        </div>
+      )
+    }
     return(
       <div className="JokeList">
+
         <div className="JokeList-sidebar">
           <h1 className="JokeList-title">
             <span>Dad</span> Jokes
@@ -44,17 +95,23 @@ class JokeList extends Component{
           <img
             src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'
           />
-          <button className="JokeList-getmore">
+          <button className="JokeList-getmore" onClick={this.handleClick}>
             New Jokes
           </button>
         </div>
 
-
         <div className="JokeList-jokes">
           {this.state.jokes.map(j=>(
-            <Joke votes={j.votes} text={j.text}/>
+            <Joke
+              key={j.id}
+              votes={j.votes}
+              text={j.text}
+              upvote={() => this.handleVote(j.id,1)}
+              downvote={() => this.handleVote(j.id,-1)}
+            />
           ))}
         </div>
+
       </div>
     )
   }
