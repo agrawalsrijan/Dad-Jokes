1d226c3ffc22568bb003aa8f685808ee7e623e22
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-14 15:59:40 +0530

    Adding more styling and animations
    Adding animations and styling to buttons

--- src/JokeList.js
+++ src/JokeList.js
@@ -85,6 +85,7 @@ class JokeList extends Component{
         </div>
       )
     }
+    let jokes = this.state.jokes.sort((a,b)=> b.votes-a.votes);
     return(
       <div className="JokeList">
 
@@ -101,7 +102,7 @@ class JokeList extends Component{
         </div>
 
         <div className="JokeList-jokes">
-          {this.state.jokes.map(j=>(
+          {jokes.map(j=>(
             <Joke
               key={j.id}
               votes={j.votes}
